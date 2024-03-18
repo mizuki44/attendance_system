be3dfd41c6685a779a74d7da6fd70ef44d08ab1d
mizuki44 <rat.raat.44@gmail.com>
2024-03-18 16:51:13 +0900

    first commit

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthorRequest;
+
+
+class AuthController extends Controller
+{
+     public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(AuthorRequest $request)
+    {
+        var_dump('test');
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    // public function index()
+    // {
+    //     return view('index');
+    // }
+    
+}
